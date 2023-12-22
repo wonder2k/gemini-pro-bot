d186491764fccb09f643600913ebd4eb63f0c658
wonder2k <46643182+wonder2k@users.noreply.github.com>
2023-12-22 20:17:47 +0800

    Update openssl.sh

--- openssl.sh
+++ openssl.sh
@@ -19,6 +19,14 @@ echo "export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64" >> ~/.bashrc
 
 openssl version
 
-./configure --enable-shared  --with-openssl=/usr
-make
-make install
+cd ..
+yum -y install bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel libffi-devel
+
+wget https://www.python.org/ftp/python/3.9.8/Python-3.9.8.tgz
+tar -zxvf Python-3.9.8.tgz
+cd Python-3.9.8
+./configure --prefix=/usr/local/python3 --enable-shared --with-openssl=/usr/local/openssl
+make && make install
+
+openssl version
+whereis openssl
